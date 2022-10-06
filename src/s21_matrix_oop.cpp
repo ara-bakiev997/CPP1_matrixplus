09efb3ba99fc32e2082d5625719390194b09b45a
Tysane Norine <tysaneno@ev-i1.kzn.21-school.ru>
2022-10-06 20:32:17 +0300

    start

--- src/s21_matrix_oop.cpp
+++ src/s21_matrix_oop.cpp
@@ -1,43 +1,45 @@
 #include "s21_matrix_oop.h"
 
+/*
+ * Parameterized constructor with number of rows and columns **/
 S21Matrix::S21Matrix(int rows, int cols) : _rows(rows), _cols(cols) {
-    matrix = (double **)new double[sizeof(double) * _rows * _cols +
-                                   sizeof(double *) * _rows];
-    double *ptr = (double *)(matrix + _rows);
-    for (int i = 0; i < _rows; i++) {
-        matrix[i] = ptr + i * _cols;
-    }
+  matrix = (double **)new double[sizeof(double) * _rows * _cols +
+								 sizeof(double *) * _rows];
+  double *ptr = (double *)(matrix + _rows);
+  for (int i = 0; i < _rows; i++) {
+	matrix[i] = ptr + i * _cols;
+  }
 }
 
 void S21Matrix::print() {
-    std::cout << this->_rows << std::endl;
-    std::cout << this->_cols << std::endl;
-    for (int i = 0; i < _rows; i++) {
-        for (int j = 0; j < _cols; j++) {
-            std::cout << matrix[i][j] << ' ';
-        }
-        std::cout << std::endl;
-    }
+  std::cout << this->_rows << std::endl;
+  std::cout << this->_cols << std::endl;
+  for (int i = 0; i < _rows; i++) {
+	for (int j = 0; j < _cols; j++) {
+	  std::cout << matrix[i][j] << ' ' ;
+	}
+	std::cout << std::endl;
+}
 }
 void S21Matrix::init(double start) {
-    for (int i = 0; i < this->_rows; i++) {
-        for (int j = 0; j < this->_cols; j++) {
-            matrix[i][j] = start++;
-        }
-    }
+  for (int i = 0; i < this->_rows; i++) {
+	for (int j = 0; j < this->_cols; j++) {
+	  matrix[i][j] = start++;
+	}
+  }
 }
 
 S21Matrix::~S21Matrix() {
-    this->_rows = 0;
-    this->_cols = 0;
-    delete[] matrix;
+  this->_rows = 0;
+  this->_cols = 0;
+  delete[] matrix;
 }
 
 int main() {
-    S21Matrix mtr(3, 3);
-    mtr.init(10);
-    mtr.print();
-    // std::cout << "hello" << std::endl;
+  S21Matrix mtr(3, 3);
+  mtr.init(10);
+  mtr.print();
+  // std::cout << "hello" << std::endl;
 
-    return 0;
+  return 0;
 }
--- src/s21_matrix_oop.h
+++ src/s21_matrix_oop.h
@@ -1,18 +1,18 @@
 #ifndef __S21MATRIX_H__
 #define __S21MATRIX_H__
-
 #include <iostream>
 
 class S21Matrix {
-private:
-    int _rows, _cols;
-    double **matrix;
-public:
-    S21Matrix(int rows, int cols);
-    ~S21Matrix();
-    void print();
-    void init(double start);
+ private:
+  int _rows, _cols;
+  double **matrix;
 
+ public:
+  S21Matrix();
+  S21Matrix(int rows, int cols);
+  ~S21Matrix();
+  void print();
+  void init(double start);
 };
 
 #endif
