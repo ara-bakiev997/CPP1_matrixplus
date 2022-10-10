905537751f05a6042764db20632042a571fbc273
tysaneno <tysaneno@student.21-school.ru>
2022-10-10 19:35:46 +0300

    new project

--- src/s21_matrix_oop.h
+++ src/s21_matrix_oop.h
@@ -1,21 +1,28 @@
-#ifndef __S21MATRIX_H__
-#define __S21MATRIX_H__
-#include <cstring>
+//
+// Created by Tysane Norine on 10/10/22.
+//
+
+#ifndef CPP1_S21_MATRIXPLUS_0_SRC_S21_MATRIX_OOP_H_
+#define CPP1_S21_MATRIXPLUS_0_SRC_S21_MATRIX_OOP_H_
+
 #include <iostream>
 
+namespace s21 {
 class S21Matrix {
-private:
+ private:
   int _rows, _cols;
   double **matrix;
 
-public:
+ public:
   S21Matrix();
   S21Matrix(int rows, int cols);
   S21Matrix(const S21Matrix &other);
-  S21Matrix(S21Matrix&& other);
+  S21Matrix(S21Matrix &&other);
   ~S21Matrix();
   void print();
   void init(double start);
 };
 
-#endif
+}// namespace s21
+
+#endif//CPP1_S21_MATRIXPLUS_0_SRC_S21_MATRIX_OOP_H_
