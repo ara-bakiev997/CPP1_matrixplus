db725d19c568beb77797282b56b0bed1ce5b1aae
tysaneno <tysaneno@student.21-school.ru>
2022-10-17 18:46:09 +0300

    old

---
+++ src/test.cc
@@ -0,0 +1,17 @@
+//
+// Created by Tysane Norine on 10/10/22.
+//
+//#include "s21_matrix_oop.h"
+//#include <iostream>
+//
+//int main() {
+//  s21::S21Matrix mtr(4, 4);
+//  mtr.init(0);
+//  mtr.print();
+//  s21::S21Matrix mtr1 = std::move(mtr);
+//
+//  //  mtr1(&mtr);
+//  mtr1.print();
+//
+//  return 0;
+//}
